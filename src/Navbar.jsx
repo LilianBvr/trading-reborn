9f948efeeb3101ecd34c4ea80eb7107f7123ea0e
Lilian Bouvier <lilianbouvier.music@gmail.com>
2020-08-04 16:12:35 +0200

    fixed navbar issues

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,37 +6,39 @@ import './Navbar.css'
 const Navbar = () => {
 
   return (
-    <div class='navbar sticky-top navbar-expand-sm navbar-light bg-light user-select-none'>
-      <Link class="navbar-brand ml-5 " to='/'>
-        <img 
-          src={require('./img/logo.png')}
-          alt="logo"
-          width='55'
-        />
-      </Link>
+    <div class='navbar-container'>
+      <div class='navbar fixed-top navbar-expand-sm navbar-light bg-light user-select-none'>
+        <Link class="navbar-brand ml-5 " to='/'>
+          <img 
+            src={require('./img/logo.png')}
+            alt="logo"
+            width='55'
+          />
+        </Link>
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse mr-3" id="navbarContent">
+        <div class="collapse navbar-collapse mr-3" id="navbarContent">
 
-          <ul class="navbar-nav ml-auto">
-              <li class='nav-item my-1 mr-sm-2 mr-xs-2'>
-                  <Link class='nav-link px-2 active' to='/'>Accueil</Link>
-              </li>
-              
-              <li class='nav-item my-1 mx-sm-2'>
-                  <Link class='nav-link px-2 active' to='/contact'>Contact</Link>
-              </li>
+            <ul class="navbar-nav ml-auto">
+                <li class='nav-item my-1 mr-sm-2 mr-xs-2'>
+                    <Link class='nav-link px-2 active' to='/'>Accueil</Link>
+                </li>
+                
+                <li class='nav-item my-1 mx-sm-2'>
+                    <Link class='nav-link px-2 active' to='/contact'>Contact</Link>
+                </li>
 
-              <li class='nav-item my-1 mx-sm-2'>
-                  <Link class='btn btn-outline-info px-2' to='/rejoindre'>Rejoindre</Link>
-              </li>
-          </ul>
+                <li class='nav-item my-1 mx-sm-2'>
+                    <Link class='btn btn-outline-info px-2' to='/rejoindre'>Rejoindre</Link>
+                </li>
+            </ul>
+
+        </div>
 
       </div>
-
     </div>
   );
 }
